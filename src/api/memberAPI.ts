326ddec440170a29a9b4bedb800eaf691940676c
Juanpi <jpablo69@hotmail.es>
2020-02-03 19:31:48 +0100

    Custom hoocks

--- src/api/memberAPI.ts
+++ src/api/memberAPI.ts
@@ -1,45 +1,43 @@
-import {MemberEntity, createDefaultMemberEntity } from '../model/member';
+import { MemberEntity, createDefaultMemberEntity } from "model/member";
 
 class MemberAPI {
+	// Just return a copy of the mock data
+	getAllMembers(organizationName: string): Promise<MemberEntity[]> {
+		const gitHubMembersUrl: string = `https://api.github.com/orgs/${organizationName}/members`;
 
-  // Just return a copy of the mock data
-  getAllMembers(organizationName : string) : Promise<MemberEntity[]> {
-    const gitHubMembersUrl : string = `https://api.github.com/orgs/${organizationName}/members`;
+		return fetch(gitHubMembersUrl)
+			.then((response) => this.checkStatus(response))
+			.then((response) => this.parseJSON(response))
+			.then((data) => this.resolveMembers(data));
+	}
 
-    return fetch(gitHubMembersUrl)
-    .then((response) => this.checkStatus(response))
-    .then((response) => this.parseJSON(response))
-    .then((data) => this.resolveMembers(data))
-    }
+	private checkStatus(response: Response): Promise<Response> {
+		if (response.status >= 200 && response.status < 300) {
+			return Promise.resolve(response);
+		} else {
+			let error = new Error(response.statusText);
+			alert(`Esta compañia no existe en nuestra base de datos: ${error}`);
+			throw error;
+		}
+	}
 
-  private checkStatus(response : Response) : Promise<Response> {
-    if (response.status >= 200 && response.status < 300) {
-      return Promise.resolve(response);
-    } else {
-      let error = new Error(response.statusText);
-      throw error;
-    }
-  }
+	private parseJSON(response: Response): any {
+		return response.json();
+	}
 
-  private parseJSON(response : Response) : any {
-    return response.json();
-  }
+	private resolveMembers(data: any): Promise<MemberEntity[]> {
+		const members = data.map((gitHubMember) => {
+			var member: MemberEntity = createDefaultMemberEntity();
 
-  private resolveMembers (data : any) : Promise<MemberEntity[]> {
+			member.id = gitHubMember.id;
+			member.login = gitHubMember.login;
+			member.avatar_url = gitHubMember.avatar_url;
 
-    const members = data.map((gitHubMember) => {
-      var member : MemberEntity = createDefaultMemberEntity();
+			return member;
+		});
 
-      member.id = gitHubMember.id;
-      member.login = gitHubMember.login;
-      member.avatar_url = gitHubMember.avatar_url;
-
-      return member;
-    });
-
-
-    return Promise.resolve(members);
-  }
+		return Promise.resolve(members);
+	}
 }
 
 export const memberAPI = new MemberAPI();
--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,58 @@ const path = require("path");
 const basePath = __dirname;
 
 module.exports = {
-  mode: "development",
-  context: path.join(basePath, "src"),
-  resolve: {
-    extensions: [".js", ".ts", ".tsx"]
-  },
-  entry: ["./main.tsx"],
-  output: {
-    path: path.join(basePath, "dist"),
-    filename: "[name].js"
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    inline: true,
-    host: "localhost",
-    port: 8080,
-    stats: "minimal"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: "awesome-typescript-loader",
-        options: {
-          useBabel: true,
-          babelCore: "@babel/core" // needed for Babel v7
-        }
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        exclude: /node_modules/,
-        loader: "file-loader",
-        options: {
-          name: "assets/img/[name].[ext]?[hash]"
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "index.html"
-    })
-  ]
+	mode: "development",
+	context: path.join(basePath, "src"),
+	resolve: {
+		alias: {
+			api: path.resolve(basePath, "./src/api"),
+			componets: path.resolve(basePath, "./src/componets"),
+			model: path.resolve(basePath, "./src/model")
+		},
+		extensions: [".js", ".ts", ".tsx"]
+	},
+	entry: ["./index.tsx"],
+	output: {
+		path: path.join(basePath, "dist"),
+		filename: "[name].js"
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		inline: true,
+		host: "localhost",
+		port: 8080,
+		stats: "minimal"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: "awesome-typescript-loader",
+				options: {
+					useBabel: true,
+					babelCore: "@babel/core" // needed for Babel v7
+				}
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				exclude: /node_modules/,
+				loader: "file-loader",
+				options: {
+					name: "assets/img/[name].[ext]?[hash]"
+				}
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "index.html"
+		})
+	]
 };
