244afdd3f80d0c8872a29b54d35f51f7f75516cd
juanpms2 <juanpablo@lemoncode.net>
2021-06-22 12:40:29 +0200

    resolved conflicts

--- src/pods/members-collection/members-collection.component.tsx
+++ src/pods/members-collection/members-collection.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
@@ -11,7 +12,6 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
-import Link from "@material-ui/core/Link";
 import Pagination from "@material-ui/lab/Pagination";
 import PaginationItem from "@material-ui/lab/PaginationItem";
 
@@ -19,7 +19,7 @@ interface Props {
 	showMembers: MemberEntity[];
 	totalMembers: number;
 	page: number;
-	handleChange: (event: Event, value: number) => void;
+	handleChange: (event: React.ChangeEvent<unknown>, value: number) => void;
 	loadMember: (login: string) => void;
 }
 
@@ -31,28 +31,28 @@ const useStyles = makeStyles((theme) => ({
 		width: "100%",
 		padding: "0 20px",
 		"& > * + *": {
-			marginTop: theme.spacing(2)
-		}
+			marginTop: theme.spacing(2),
+		},
 	},
 	paper: {
 		padding: theme.spacing(2),
 		textAlign: "center",
-		color: theme.palette.text.secondary
+		color: theme.palette.text.secondary,
 	},
 	pagination: {
-		margin: "30px auto"
+		margin: "30px auto",
 	},
 	carRoot: {
-		maxWidth: 345
+		maxWidth: 345,
 	},
 	media: {
-		height: 140
+		height: 140,
 	},
 	large: {
 		width: theme.spacing(18),
 		height: theme.spacing(18),
-		margin: "5% auto"
-	}
+		margin: "5% auto",
+	},
 }));
 
 export const MembersCollectionComponent: React.FunctionComponent<Props> = (
